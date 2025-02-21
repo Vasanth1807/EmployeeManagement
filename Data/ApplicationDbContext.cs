2d08c7ad395092d4273383604ec07821b79fe33b
Vasanthakumar <145379716+Vasanth1807@users.noreply.github.com>
2025-02-21 19:08:22 +0530

    WholeFile

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EmployeeManagemnet.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagemnet.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,33 @@
+﻿namespace EmployeeManagemnet.Models
+{
+    public class Employee : UserActivity
+    {
+        public int Id { get; set; }
+
+        public string EmpNo { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FUllName => $"{FirstName} {MiddleName} {LastName}";
+
+        public int PhoneNUmber { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string Country { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Address { get; set; }
+
+        public string Department { get; set; }
+
+        public string Designation { get; set; }
+
+
+    }
+}
